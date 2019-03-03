2edfd9d38d8386a36b4e06a79ae67432e043893f
Nikolaos Stamatopoulos <stamatopoulosnikos@gmail.com>
2019-03-03 21:28:15 +0100

    Remove unused methods, improve comments

--- main.rb
+++ main.rb
@@ -14,12 +14,14 @@ class HashCode
   def run
     parse_input
     create_tags_map
+
     solve
+
+    validate_output
     score_output
     write_output
   end
 
-  # GOOD
   def parse_input
     File.readlines(@in_file).each_with_index do |line, idx|
       if idx == 0
@@ -30,30 +32,9 @@ class HashCode
       image = line.strip.split
       @images << { id: idx - 1, orientation: image.first, tags: image.drop(2) }
     end
+
+    # initialize an array to lookup for used images
     @used_map = Array.new(@num_images) { |i| false }
-  end
-
-  # GOOD
-  def score_output
-    puts("Validating result...")
-    slides_unique = @slides.uniq
-    is_invalid = (@slides.length != slides_unique.length) || (@slides & slides_unique != @slides)
-
-    if is_invalid
-      puts("INVALID: Result contains duplicates.")
-      exit
-    end
-
-    puts("Result is VALID! Calculating score...")
-    score = 0
-
-    for idx in 1..@slides.count - 1
-      score += get_score_between(@slides[idx - 1], @slides[idx])
-    end
-
-    pp "Slides: #{@slides.count}"
-    pp "Score: #{score}"
-    score
   end
 
   def solve
@@ -63,19 +44,24 @@ class HashCode
     @slides << @images[0]
     @used_map[0] = true
 
+    # we need to append (num_images - 1) images
     for i in 0..@num_images - 2
-      # just a progress indicator to see where we are at
+
+      # just a dumb progress indicator to see where we are
       i % 1000 == 0 && puts(i)
 
-      found_id = maybe_get_next_id(i)
+      # try to find next id according to tags
+      found_id = find_next(i)
+      # if not found, set to first unused image
       found_id ||= @used_map.find_index(false)
 
+      # push
       @slides << @images[found_id]
       @used_map[found_id] = true
     end
   end
 
-  def maybe_get_next_id(i)
+  def find_next(i)
     @slides[i][:tags].each do |tag|
       found_id = @tags[tag].detect { |id| id != @slides[i][:id] && @used_map[id] == false }
       if found_id
@@ -85,9 +71,8 @@ class HashCode
     nil
   end
 
-  # GOOD
   def create_tags_map
-    puts("Creating tag map...")
+    puts("Mapping ids to tags...")
     @images.each_with_index do |image, id|
       image[:tags].each do |tag|
         if @tags.key?(tag)
@@ -99,24 +84,34 @@ class HashCode
     end
   end
 
-  # GOOD
-  def get_unique_tags(tags)
-    tags.select { |tag| @tags[tag].count == 1 }
-  end
+  def validate_output
+    puts "Validating result..."
+    slides_unique = @slides.uniq
+    is_invalid = (@slides.length != slides_unique.length) || (@slides & slides_unique != @slides)
 
-  # GOOD
-  def get_non_unique_tags(tags)
-    tags.reject { |tag| @tags[tag].count == 1 }
-  end
-
-  # GOOD
-  def get_images_with_unique_tags
-    @images.select do |image|
-      image[:tags].all? { |tag| @tags[tag].count == 1 }
+    if is_invalid
+      puts "INVALID: Result contains duplicates."
+      exit
     end
+
+    # TODO: add checks for vertical image pairs
+
+    puts "Result is VALID!"
   end
 
-  # GOOD
+  def score_output
+    puts "Calculating score..."
+    score = 0
+
+    for i in 1..@slides.count - 1
+      score += get_score_between(@slides[i - 1], @slides[i])
+    end
+
+    puts "Slides: #{@slides.count}"
+    puts "Score: #{score}"
+    score
+  end
+
   def write_output
     puts("Writing to file...")
     File.open(@out_file, 'w') do |f|
@@ -127,7 +122,6 @@ class HashCode
     end
   end
 
-  # GOOD
   def get_score_between(slide1, slide2)
     num_common = (slide1[:tags] & slide2[:tags]).count
     num_excl1 = (slide1[:tags] - slide2[:tags]).count
